872af161587322a6d62d745fe0599623388104b5
oghenetega16 <sukuruoghenetega@gmail.com>
2025-09-08 11:45:16 +0100

    Header component is ready

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import SearchPanel from './components/SearchPanel';
 
 export default function App() {
     return (
-        <main className="bg-[#02012B] p-4 h-screen">
+        <main className="bg-[#02012B] p-4 h-screen sm:p-8">
             <Header />
             <SearchPanel />
         </main>
--- src/components/SearchPanel.tsx
+++ src/components/SearchPanel.tsx
@@ -3,13 +3,13 @@ import searchUrl from '../../public/assets/images/icon-search.svg';
 export default function App() {
     return (
         <div className="font-dmsans text-center mt-12">
-            <h1 className="font-bricolage text-5xl font-extrabold mb-10 leading-15">How's the sky looking today?</h1>
-            <form>
-                <div className='relative'>
+            <h1 className="font-bricolage text-5xl font-extrabold mb-10 leading-15 w-75 mx-auto sm:w-full">How's the sky looking today?</h1>
+            <form className="flex flex-col gap-3 sm:flex-row sm:space-y-0 md:w-xl md:mx-auto">
+                <div className='relative sm:w-full'>
                     <input type="text" placeholder="Search for a place..." className="bg-neutral-600 w-full rounded-lg py-3 px-14" />
                     <img src={searchUrl} alt="search icon" className="absolute top-3 left-5" />
                 </div>
-                <button className="w-full bg-blue-500 rounded-lg py-3 my-3">Search</button>
+                <button className="w-full bg-blue-500 rounded-lg py-3 sm:w-1/6">Search</button>
             </form>
         </div>
     )
